f0b6b098629d6634c701cc9e31f0d9fb38ffb6b7
Piotr Jalocha <darricck04@gmail.com>
2018-12-07 19:14:28 +0100

    delet dev kesy

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,18 +1,18 @@
 
-import key from './devKeys';
+// import key from './devKeys';
 import keyPro from './prodKeys';
 
 
 let keys = {};
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    keys = {
-        key: key.keyTMDBapi
-    }
-} else {
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+//     keys = {
+//         key: key.keyTMDBapi
+//     }
+// } else {
     keys = {
         key: keyPro.keyTMDBapi
     }
-}
+// }
 
 export default keys;
